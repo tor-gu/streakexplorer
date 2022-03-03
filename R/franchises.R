67d6f4f6c38b1ff7794355c0514d2536e029a066
Tor Gunston <tor.gunston@gmail.com>
2022-03-03 12:55:34 -0500

    1) styler on franchises.R 2) styler on plot.R

--- R/franchises.R
+++ R/franchises.R
@@ -1,11 +1,12 @@
 franchises_by_season <- function(franchises, year) {
   franchises %>% dplyr::filter(FirstSeason <= year &
-                                 (FinalSeason >= year | is.na(FinalSeason)))
+    (FinalSeason >= year | is.na(FinalSeason)))
 }
 
 # TODO maybe delete
 franchises_by_seasons <- function(franchises, years) {
   purrr::map(years, function(year) franchises_by_season(franchises, year)) %>%
-    data.table::rbindlist() %>% tibble::as_tibble() %>% unique()
+    data.table::rbindlist() %>%
+    tibble::as_tibble() %>%
+    unique()
 }
-
