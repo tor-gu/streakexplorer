a00bd88a33782645e2413c4df91a146cdff0017e
Tor Gunston <tor.gunston@gmail.com>
2022-03-03 09:25:58 -0500

    Applied styler to app.R

--- R/app.R
+++ R/app.R
@@ -1,28 +1,35 @@
 library(shiny)
 
 
-initialYearRange <- c(1948,1949)
+initialYearRange <- c(1948, 1949)
 initialYears <- initialYearRange[[1]]:initialYearRange[[2]]
-theme <- bslib::bs_theme(bootswatch="slate",
-                         heading_font = "1.2",
-                         font_scale = 0.8)
+theme <- bslib::bs_theme(
+  bootswatch = "slate",
+  heading_font = "1.2",
+  font_scale = 0.8
+)
 ui <- fluidPage(
   shinyjs::useShinyjs(),
   tags$style("#game_log td, th {padding: 0; text-align: right}"),
   tags$style("#streak_summary td, th {padding: 0; text-align: right}"),
-  theme=theme,
-  #theme=bslib::bs_theme(),
+  theme = theme,
+  # theme=bslib::bs_theme(),
   titlePanel("Streak Explorer"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("years", "Years", min=1948, max=2021, step=1,
-                  value=initialYearRange, sep=""),
+      sliderInput("years", "Years",
+        min = 1948, max = 2021, step = 1,
+        value = initialYearRange, sep = ""
+      ),
       selectInput("leagues", "League",
-                  choices = c("All Leagues" = "BOTH", "AL"="AL", "NL"="NL")),
-      selectInput("divisions", "Divisions", choices=list(), multiple=TRUE),
-      selectInput("teams", "Teams", choices=list(), multiple=TRUE),
-      radioButtons("streak_type", "Streak Type", choices=c("HOT", "COLD"),
-                   selected="HOT")
+        choices = c("All Leagues" = "BOTH", "AL" = "AL", "NL" = "NL")
+      ),
+      selectInput("divisions", "Divisions", choices = list(), multiple = TRUE),
+      selectInput("teams", "Teams", choices = list(), multiple = TRUE),
+      radioButtons("streak_type", "Streak Type",
+        choices = c("HOT", "COLD"),
+        selected = "HOT"
+      )
     ),
     mainPanel(
       plotly::plotlyOutput(outputId = "streaks"),
@@ -34,9 +41,11 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  #bslib::bs_themer()
+  # bslib::bs_themer()
 
-  hot <- reactive({input$streak_type == "HOT"})
+  hot <- reactive({
+    input$streak_type == "HOT"
+  })
 
   base_lines <- reactive({
     message("loading base lines")
@@ -65,7 +74,9 @@ server <- function(input, output, session) {
     }
   })
 
-  selected_years <- reactive({years()[[1]]:years()[[2]]})
+  selected_years <- reactive({
+    years()[[1]]:years()[[2]]
+  })
   selected_leagues <- reactiveVal(c("AL", "NL"))
   selected_league_divisions <- reactiveVal(list())
   no_division_choices <- reactive({
@@ -100,28 +111,37 @@ server <- function(input, output, session) {
   }
   observeEvent(input$leagues, {
     selected_leagues(
-      if(input$leagues == "BOTH") c("AL", "NL") else input$leagues)
-    updateSelectInput(session, "divisions", choices=division_choices(),
-                      selected=unlist(division_choices()))
+      if (input$leagues == "BOTH") c("AL", "NL") else input$leagues
+    )
+    updateSelectInput(session, "divisions",
+      choices = division_choices(),
+      selected = unlist(division_choices())
+    )
     check_no_division()
   })
 
   observeEvent(years(), {
-    updateSelectInput(session, "divisions", choices=division_choices(),
-                      selected=unlist(division_choices()))
+    updateSelectInput(session, "divisions",
+      choices = division_choices(),
+      selected = unlist(division_choices())
+    )
     check_no_division()
   })
 
   observeEvent(input$divisions, {
     selected_league_divisions(
-      input$divisions %>% division_choice_values_as_league_and_division_list())
-    updateSelectInput(session, "teams", choices=teams_choices(),
-                      selected=teams_choices())
-
+      input$divisions %>% division_choice_values_as_league_and_division_list()
+    )
+    updateSelectInput(session, "teams",
+      choices = teams_choices(),
+      selected = teams_choices()
+    )
   })
 
   max_rank <- reactiveVal(10)
-  years <- reactive({input$years}) %>% debounce(333)
+  years <- reactive({
+    input$years
+  }) %>% debounce(333)
 
   filtered_lines <- reactive({
     req(years(), input$teams)
@@ -130,11 +150,10 @@ server <- function(input, output, session) {
       dplyr::filter(Year >= years()[[1]] & Year <= years()[[2]]) %>%
       dplyr::filter(Team %in% input$teams)
     max_rank(filtered %>%
-               dplyr::group_by(IntensityLevel) %>%
-               dplyr::slice_min(Rank, n=10) %>%
-               dplyr::ungroup() %>%
-               dplyr::summarise(ms=max(Rank)) %>% dplyr::pull(ms)
-    )
+      dplyr::group_by(IntensityLevel) %>%
+      dplyr::slice_min(Rank, n = 10) %>%
+      dplyr::ungroup() %>%
+      dplyr::summarise(ms = max(Rank)) %>% dplyr::pull(ms))
     filtered %>%
       dplyr::filter(Rank <= max_rank()) %>%
       lines_remove_nubs()
@@ -155,8 +174,8 @@ server <- function(input, output, session) {
   })
 
   selected_line_id <- reactiveVal(NULL)
-  observeEvent(plotly::event_data("plotly_click", source="lines_plot"), {
-    click_data <- plotly::event_data("plotly_click", source="lines_plot")
+  observeEvent(plotly::event_data("plotly_click", source = "lines_plot"), {
+    click_data <- plotly::event_data("plotly_click", source = "lines_plot")
     if (is.null(click_data)) {
       selected_line_id(NULL)
     } else {
@@ -180,51 +199,65 @@ server <- function(input, output, session) {
   highlight_data <- reactive({
     message("About to do highlighting")
     print(selected_line_id())
-    lines_highlight(filtered_lines(), concordances(),
-                    lines_to_streaks(), selected_line_id())
+    lines_highlight(
+      filtered_lines(), concordances(),
+      lines_to_streaks(), selected_line_id()
+    )
   })
 
   output$streaks <- plotly::renderPlotly({
     message("Rendering plotly...")
-    highlight_data() %>% plot_lines(max_rank(),
-                                    input$streak_type == "COLD")
+    highlight_data() %>% plot_lines(
+      max_rank(),
+      input$streak_type == "COLD"
+    )
   })
 
   output$streak_summary <- DT::renderDT({
     message(paste("Rendering summary table...", selected_streak_id()))
-    if (is.null(selected_streak_id())) return(NULL)
+    if (is.null(selected_streak_id())) {
+      return(NULL)
+    }
 
-    summary <- streak_summary_data(selected_streak_id(),
-                                   isolate(streaks()),
-                                   SOMData::game_logs)
+    summary <- streak_summary_data(
+      selected_streak_id(),
+      isolate(streaks()),
+      SOMData::game_logs
+    )
     DT::datatable(
       summary$data,
-      caption=summary$caption,
+      caption = summary$caption,
       rownames = FALSE,
-      options=list(ordering=FALSE,
-                   paging=FALSE,
-                   searching=FALSE,
-                   info=FALSE
+      options = list(
+        ordering = FALSE,
+        paging = FALSE,
+        searching = FALSE,
+        info = FALSE
       )
     )
   })
 
   output$game_log <- DT::renderDT({
     message(paste("Rendering game log table...", selected_streak_id()))
-    if (is.null(selected_streak_id())) return(NULL)
+    if (is.null(selected_streak_id())) {
+      return(NULL)
+    }
 
-    game_log <- streak_game_log_data(selected_streak_id(),
-                                     isolate(streaks()),
-                                     SOMData::game_logs)
+    game_log <- streak_game_log_data(
+      selected_streak_id(),
+      isolate(streaks()),
+      SOMData::game_logs
+    )
     DT::datatable(
       game_log$data,
-      caption=game_log$caption,
+      caption = game_log$caption,
       rownames = FALSE,
-      options(ordering=FALSE, searching=FALSE, pageLength=25,
-              lengthChange=FALSE)
+      options(
+        ordering = FALSE, searching = FALSE, pageLength = 25,
+        lengthChange = FALSE
+      )
     )
   })
-
 }
 
 shinyApp(ui, server)
