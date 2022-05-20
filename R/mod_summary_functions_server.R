bbbd6bd19ea2424b83d01b8e302a360759e85e79
tor-gu <tor.gunston@protonmail.com>
2022-05-20 14:01:45 -0400

    Modularize: ss_get_standings_for_streak

--- R/mod_summary_functions_server.R
+++ R/mod_summary_functions_server.R
@@ -23,7 +23,7 @@ summary_server_get_streak_standings <- function(franchises, selected_streak) {
   withCallingHandlers({
     lzy_standings <- dplyr::tbl(se_pool, "standings")
     lzy_game_logs <- dplyr::tbl(se_pool, "game_logs")
-    streaks_get_standings(lzy_standings, lzy_game_logs, franchises,
+    ss_get_standings_for_streak(lzy_standings, lzy_game_logs, franchises,
                           selected_streak)
   }, warning = function(wrn) {
     if (stringr::str_starts(wrn$message, "Decimal MySQL")) {
@@ -304,4 +304,84 @@ ss_plot_standings_graph <- function(standings, team, start_date, end_date) {
     ggplot2::scale_y_continuous(breaks=0, limits=c(y_min,y_max))
 }
 
+#' ss_get_standings_for_streak
+#'
+#' Given a streak, return the division standings before the streak started
+#' (`standings_before`), after the last game of the streak (`standings_after`),
+#' and at the end of the season (`standings_final`). Also compute a summary
+#' object (`streak_info`).
+#'
+#' @param lzy_standings Lazy standings table
+#' @param lzy_game_logs Lazy game logs table
+#' @param franchises Franchises table
+#' @param streak Streak
+#'
+#' @return list with `streak_info`, `standings_before`, `standings_after`,
+#' and `standings_final`
+ss_get_standings_for_streak <- function(lzy_standings, lzy_game_logs,
+                                  franchises, streak) {
+  # Get the division
+  division <- franchises %>%
+    franchises_get_division_by_team_year(streak$Team, streak$Year)
+  division_teams <- division$teams$TeamID
+
+  # Set up a query for all games for all teams in the division for the year.
+  lzy_division_season_games <-
+    lzy_game_logs %>%
+    dplyr::filter(Year==local(streak$Year), Team %in% division_teams)
+
+  # Get the first game and the last game
+  first_game <- lzy_division_season_games %>%
+    dplyr::filter(Year==local(streak$Year),
+                  Team==local(streak$Team),
+                  GameIndex==local(streak$LoIndex)) %>%
+    dplyr::collect()
+  last_game <-  lzy_division_season_games %>%
+    dplyr::filter(Year==local(streak$Year),
+                  Team==local(streak$Team),
+                  GameIndex==local(streak$HiIndex)) %>%
+    dplyr::collect()
+  # Calculate the standings before
+  standings_before <-
+    standings_get_by_season_game_id(lzy_standings,
+                                    lzy_division_season_games,
+                                    division$division,
+                                    first_game$SeasonGameId,
+                                    before=TRUE) %>%
+    dplyr::left_join(division$teams, by=c("Team"="TeamID")) %>%
+    dplyr::select(Team, Wins,Losses, Ties, GB, Location, Nickname) %>%
+    dplyr::arrange(GB)
+  # Calculate the standings after
+  standings_after <-
+    standings_get_by_season_game_id(lzy_standings,
+                                    lzy_division_season_games,
+                                    division$division,
+                                    last_game$SeasonGameId,
+                                    before=FALSE) %>%
+    dplyr::left_join(division$teams, by=c("Team"="TeamID")) %>%
+    dplyr::select(Team, Wins,Losses, Ties, GB, Location, Nickname) %>%
+    dplyr::arrange(GB)
+  # Look up the final standings
+  standings_final <- standings_get_final_standings(lzy_standings,
+                                                   division$division) %>%
+    dplyr::left_join(division$teams, by=c("Team"="TeamID")) %>%
+    dplyr::select(Team, Wins,Losses, Ties, GB, Location, Nickname) %>%
+    dplyr::arrange(GB)
+
+  # Gather summary streak info
+  streak_info <- division$division %>%
+    dplyr::mutate(Year=local(streak$Year),
+                  Team=local(streak$Team),
+                  Start=local(first_game$Date),
+                  End=local(last_game$Date)) %>%
+    dplyr::left_join(division$teams, by=c("Team"="TeamID"))
+
+  # Assemble the result
+  list(
+    streak_info=streak_info,
+    standings_before=standings_before,
+    standings_final=standings_final,
+    standings_after=standings_after
+  )
+}
 
--- tests/testthat/test-mod_summary_functions_server.R
+++ tests/testthat/test-mod_summary_functions_server.R
@@ -63,4 +63,60 @@ test_that("ss_streak_game_log_data works with 'completions'", {
   expect_equal(actual$data, expected_data)
 })
 
+test_that("ss_get_standings_for_streak works with 'completions'", {
+  with_mock_db({
+    mock_conn <- suppressWarnings(
+      dbConnect(RMySQL::MySQL(), dbname="streak_explorer_data")
+    )
+    lzy_game_logs <- dplyr::tbl(mock_conn, "game_logs")
+    lzy_standings <- dplyr::tbl(mock_conn, "standings")
+    franchises <- dplyr::tbl(mock_conn, "franchises") %>% dplyr::collect()
+    streak <- list(Year=2004, Team="CIN", LoIndex=100, HiIndex=105)
+    actual <- ss_get_standings_for_streak(lzy_standings, lzy_game_logs, franchises,
+                                          streak)
+    dbDisconnect(mock_conn)
+  })
+  expected_streak_info <- tibble::tibble(
+    League="NL",
+    Division="Central",
+    Year=2004,
+    Team="CIN",
+    Start="2004-07-26",
+    End="2004-07-31",
+    Location="Cincinnati",
+    Nickname="Reds"
+  )
+  expected_standings_before <- tibble::tribble(
+    ~Team, ~Wins, ~Losses, ~Ties, ~GB, ~Location,    ~Nickname,
+    "SLN", 62,    36,      0,     0,   "St. Louis",  "Cardinals",
+    "CHN", 52,    46,      0,    10,   "Chicago",    "Cubs",
+    "CIN", 50,    49,      0,  12.5,   "Cincinnati", "Reds",
+    "HOU", 49,    49,      0,    13,   "Houston",    "Astros",
+    "MIL", 48,    49,      0,  13.5,   "Milwaukee",  "Brewers",
+    "PIT", 47,    49,      0,    14,   "Pittsburgh", "Pirates"
+  )
+  expected_standings_after <- tibble::tribble(
+    ~Team, ~Wins, ~Losses, ~Ties, ~GB, ~Location,    ~Nickname,
+    "SLN", 66,    37,      0,     0,   "St. Louis",  "Cardinals",
+    "CHN", 56,    48,      0,  10.5,   "Chicago",    "Cubs",
+    "HOU", 52,    52,      0,  14.5,   "Houston",    "Astros",
+    "CIN", 51,    53,      0,  15.5,   "Cincinnati", "Reds",
+    "MIL", 50,    53,      0,    16,   "Milwaukee",  "Brewers",
+    "PIT", 49,    53,      0,  16.5,   "Pittsburgh", "Pirates"
+  )
+  expected_standings_final <- tibble::tribble(
+    ~Team, ~Wins, ~Losses, ~Ties, ~GB, ~Location,    ~Nickname,
+    "SLN", 105,    57,  0,     0,   "St. Louis",  "Cardinals",
+    "HOU", 92,     70,  0,    13,   "Houston",    "Astros",
+    "CHN", 89,     73,  0,    16,   "Chicago",    "Cubs",
+    "CIN", 76,     86,  0,    29,   "Cincinnati", "Reds",
+    "PIT", 72,     89,  0,  32.5,   "Pittsburgh", "Pirates",
+    "MIL", 67,     94,  0,  37.5,   "Milwaukee",  "Brewers",
+  )
+  expect_equal(actual$streak_info, expected_streak_info)
+  expect_equal(actual$standings_before, expected_standings_before)
+  expect_equal(actual$standings_after,  expected_standings_after)
+  expect_equal(actual$standings_final,  expected_standings_final)
+})
+
 
