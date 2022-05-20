2f0ed82200ed8b3662f22738f37f593628fb547b
tor-gu <tor.gunston@protonmail.com>
2022-05-20 10:26:29 -0400

    Modularizing: filter_ui_filter_by_years filter_ui_filter_by_league filter_ui_truncate_years filter_ui_get_divisions filter_ui_generate_division_selection filter_ui_generate_league_division_selection filter_ui_division_as_choice_value filter_ui_division_as_choice_label

--- R/mod_filter_functions_ui.R
+++ R/mod_filter_functions_ui.R
@@ -48,10 +48,227 @@ filter_ui_division_choice_value_as_league_and_division <-
 #' @return List of choices for the UI
 filter_ui_build_divisions_choices <- function(franchises, years, leagues) {
   franchises %>%
-    ui_filter_by_years(years) %>%
-    ui_filter_by_league(leagues) %>%
-    ui_truncate_years(years) %>%
-    ui_get_divisions() %>%
-    ui_generate_division_selection()
+    filter_ui_filter_by_years(years) %>%
+    filter_ui_filter_by_league(leagues) %>%
+    filter_ui_truncate_years(years) %>%
+    filter_ui_get_divisions() %>%
+    filter_ui_generate_division_selection()
+}
+
+#' filter_ui_filter_by_years
+#'
+#' Filter the franchises table by a set of years. Return franchises matching
+#' any year in this list (but the list must consist of consecutive integers).
+#'
+#' @param franchises  Franchises table
+#' @param years vector of years.
+#'
+#' @return Filtered franchises table
+filter_ui_filter_by_years <- function(franchises, years) {
+  min_year <- years[[1]]
+  max_year <- years[[length(years)]]
+  franchises %>% dplyr::filter(FirstSeason <= max_year,
+                               (is.na(FinalSeason) | FinalSeason >= min_year))
+}
+
+#' filter_ui_filter_by_league
+#'
+#' Filter the franchises table by leagues
+#'
+#' @param franchises Franchises table
+#' @param leagues vector of leagues
+#'
+#' @return Filtered franchises table
+filter_ui_filter_by_league <- function(franchises, leagues) {
+  franchises %>% dplyr::filter(League %in% leagues)
+}
+
+#' filter_ui_truncate_years
+#'
+#' Given a franchises table and a set of year, modify rows of the table so that
+#' the `FirstSeason` and `FinalSeason` are 'truncated' by the min and max of the
+#' years.  For example, if a row has `FinalSeason` `NA` or 2000, and the max
+#' value in `years` is 1990, change the `FinalSeason` to 1990.
+#'
+#' The assumption is that the `franchises` table has already been filtered by
+#' the `years` list.
+#'
+#' @param franchises  Franchises table
+#' @param years vector of years.
+#'
+#' @return Modified franchises table
+filter_ui_truncate_years <- function(franchises, years) {
+  franchises %>% dplyr::mutate(
+    FirstSeason = pmax(FirstSeason, min(years)),
+    FinalSeason = pmin(max(years), FinalSeason, na.rm = TRUE)
+  )
+}
+
+#' filter_ui_get_divisions
+#'
+#' Given a franchises table (which as already been truncated to to a
+#' range of years), return all the league/division combos, as well
+#' as the first and final seasons.
+#'
+#' @param franchises Franchise table
+#'
+#' @return Table with `League`, `Division`, `FirstSeason`, `FinalSeason`
+filter_ui_get_divisions <- function(franchises) {
+  franchises %>%
+    dplyr::group_by(League, Division) %>%
+    dplyr::summarise(
+      FirstSeason = min(FirstSeason), FinalSeason = max(FinalSeason),
+      .groups = "drop"
+    )
+}
+
+#' filter_ui_generate_division_selection
+#'
+#' Given a division table, generate a choice list for the division selection
+#' UI.  If all the divisions are from the same league, there will be an
+#' element for each division. If both leagues are represented, then there will
+#' be two sublists, labeled "AL Divisions" and "NL Divisions".  (This will
+#' generate separators in the UI)
+#'
+#' @param division_table Division table
+#'
+#' @return List of choices for the UI
+filter_ui_generate_division_selection <- function(division_table) {
+  # Get the leagues
+  leagues <- division_table %>%
+    dplyr::pull(League) %>%
+    unique()
+  # We need the treat the single-league case differently than the multi-league
+  # case: With a single league, we just return a list of choices.  But if there
+  # are multiple leagues, we want to return a list-of-lists, so that we have
+  # divisions.
+  if (length(leagues) == 1) {
+    # Single league
+    filter_ui_generate_league_division_selection(division_table, leagues)
+  } else {
+    # Both leagues
+    result <- purrr::map(
+      leagues,
+      ~ filter_ui_generate_league_division_selection(division_table, .)
+    )
+    # Add the separator labels
+    names(result) <- glue::glue("{leagues} Divisions")
+    result
+  }
+}
+
+#' filter_ui_generate_league_division_selection
+#'
+#' Given a division table and a league return a named list of choices, which
+#' may look something like this:
+#' ```
+#' list("AL East (1969-1985)"="AL_East",
+#'      "AL West (1980-2021)"="AL_West")
+#' ```
+#'
+#' @param division_table Division table
+#' @param league League to filter by
+#'
+#' @return Named list
+filter_ui_generate_league_division_selection <- function(division_table, league) {
+  # Filter to just the league we care arout
+  league_divisions <- division_table %>%
+    dplyr::filter(League == league)
+
+  # The choice values are handled by filter_ui_division_as_choice_value
+  choices <- purrr::pmap(
+    league_divisions,
+    function(League, Division, ...) {
+      filter_ui_division_as_choice_value(League, Division)
+    }
+  )
+
+  # Add the choice names
+  if (torgutil::tbl_is_column_value_unique(league_divisions, FirstSeason) &&
+      torgutil::tbl_is_column_value_unique(league_divisions, FinalSeason)) {
+    # All the year ranges are the same, so we don't need to include those
+    names(choices) <- purrr::pmap(
+      league_divisions, function(League, Division, FirstSeason, FinalSeason) {
+        filter_ui_division_as_choice_label(League, Division)
+      }
+    )
+  } else {
+    # There is some variation, so we need to include year ranges in the names
+    # (e.g. "AL East (1969-2021)")
+    min_first_season <- min(league_divisions$FirstSeason)
+    max_final_season <- max(league_divisions$FinalSeason)
+    names(choices) <- purrr::pmap(
+      league_divisions, function(League, Division, FirstSeason, FinalSeason) {
+        filter_ui_division_as_choice_label(
+          League, Division, FirstSeason, FinalSeason,
+          min_first_season, max_final_season
+        )
+      }
+    )
+  }
+
+  # Return the result
+  choices
+}
+
+#' filter_ui_division_as_choice_value
+#'
+#' Build a string like "AL_West" or "NL_None"
+#'
+#' @param league League
+#' @param division Division, or `NA`
+#'
+#' @return String of the form League_Division
+filter_ui_division_as_choice_value <- function(league, division) {
+  if (is.na(division)) {
+    glue::glue("{league}_None")
+  } else {
+    glue::glue("{league}_{division}")
+  }
+}
+
+#' filter_ui_division_as_choice_label
+#'
+#' Given a league and division and, optionally, first and final years, as well
+#' as a year range, produce strings like this:
+#' * "AL East"
+#' * "AL East (1990-1995)
+#' * "AL No Division"
+#' * "AL No Division (1990-1995)"
+#' If the first and final year is contained entirely within the year range,
+#' then just the league and division will be returned.  But if the min
+#' or max year extends beyond the year range, then a range of years will
+#' be appended.
+#' @param league League
+#' @param division Division (or `NA`)
+#' @param first_year First year the division is active
+#' @param final_year Final year the division is active
+#' @param min_first_year First year of the year range
+#' @param max_final_year Final year of the year range
+#'
+#' @return League/Division/Year string
+filter_ui_division_as_choice_label <- function(league, division,
+                                               first_year = NULL,
+                                               final_year = NULL,
+                                               min_first_year = NULL,
+                                               max_final_year = NULL) {
+  # The 'year part' of the return value is only included if
+  # the first or final year extend outside of the year-range.
+  if ((is.null(first_year) & is.null(final_year)) ||
+      (first_year == min_first_year & final_year == max_final_year)) {
+    year_part <- ""
+  } else {
+    year_part <- glue::glue(" ({first_year}-{final_year})", .null = "")
+  }
+
+  # If the division is NA, use the string "No Division"
+  if (is.na(division)) {
+    division_part <- "No Division"
+  } else {
+    division_part <- division
+  }
+
+  # Assemble the result
+  glue::glue("{league} {division_part}{year_part}")
 }
 
--- tests/testthat/test-mod_filter_functions_ui.R
+++ tests/testthat/test-mod_filter_functions_ui.R
@@ -30,3 +30,245 @@ test_that("filter_ui_build_divisions_choices basic test", {
   expect_equal(actual, expected)
 })
 
+test_that("filter_ui_filter_by_years handles basic scenarios", {
+  franchises <- tibble::tribble(
+    ~FranchiseID, ~FirstSeason, ~FinalSeason,
+    "AA1",        1,            10,
+    "AA2",        11,           20,
+    "AA3",        21,           NA,
+    "BB1",        15,           25,
+    "BB2",        26,           35,
+    "BB3",        36,           NA
+  )
+
+  years_0_40 <- filter_ui_filter_by_years(franchises, 0:40) %>%
+    dplyr::pull(FranchiseID)
+  years_5_15 <- filter_ui_filter_by_years(franchises, 5:15) %>%
+    dplyr::pull(FranchiseID)
+  years_25_40 <- filter_ui_filter_by_years(franchises, 25:40) %>%
+    dplyr::pull(FranchiseID)
+
+  expect_setequal(years_0_40, c("AA1","AA2","AA3","BB1","BB2","BB3"))
+  expect_setequal(years_5_15, c("AA1","AA2","BB1"))
+  expect_setequal(years_25_40, c("AA3","BB1","BB2","BB3"))
+})
+
+test_that("filter_ui_filter_by_league handles basic scenarios", {
+  franchises <- tibble::tribble(
+    ~FranchiseID, ~League,
+    "AA1",        "AL",
+    "AA2",        "AL",
+    "AA3",        "AL",
+    "BB1",        "NL",
+    "BB2",        "NL",
+    "BB3",        "NL",
+  )
+
+  empty <- filter_ui_filter_by_league(franchises, character(0)) %>%
+    dplyr::pull(FranchiseID)
+  al <- filter_ui_filter_by_league(franchises, "AL")  %>%
+    dplyr::pull(FranchiseID)
+  nl <- filter_ui_filter_by_league(franchises, "NL") %>%
+    dplyr::pull(FranchiseID)
+  both <- filter_ui_filter_by_league(franchises, c("AL", "NL")) %>%
+    dplyr::pull(FranchiseID)
+
+
+  expect_equal(length(empty), 0)
+  expect_setequal(al, c("AA1","AA2","AA3"))
+  expect_setequal(nl, c("BB1","BB2","BB3"))
+  expect_setequal(both, c("AA1", "AA2", "AA3","BB1","BB2","BB3"))
+})
+
+test_that("filter_ui_truncate_years handles basic scenarios", {
+  franchises <- tibble::tribble(
+    ~FranchiseID, ~FirstSeason, ~FinalSeason,
+    "AA1",        1,            10,
+    "AA2",        11,           20,
+    "AA3",        21,           NA
+  )
+
+  actual <- filter_ui_truncate_years(franchises, 5:25)
+  expected <- tibble::tribble(
+    ~FranchiseID, ~FirstSeason, ~FinalSeason,
+    "AA1",        5,            10,
+    "AA2",        11,           20,
+    "AA3",        21,           25
+  )
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_get_divisions handles divisions", {
+  franchises <- tibble::tribble(
+    ~FranchiseID, ~League, ~Division,  ~FirstSeason, ~FinalSeason,
+    "ALE1",       "AL",     "East",    1,            20,
+    "ALE2",       "AL",     "East",    10,           30,
+    "ALW1",       "AL",     "West",    5,            25,
+    "ALW2",       "AL",     "West",    15,           35,
+  )
+  actual <- filter_ui_get_divisions(franchises)
+  expected <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    "East",    1,            30,
+    "AL",    "West",    5,            35
+  )
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_get_divisions handles non-divisions", {
+  franchises <- tibble::tribble(
+    ~FranchiseID, ~League, ~Division,  ~FirstSeason, ~FinalSeason,
+    "AL1",        "AL",     NA,        1,            20,
+    "AL2",        "AL",     NA,        10,           30,
+    "NL1",        "NL",     NA,        5,            25,
+    "NL2",        "NL",     NA,        15,           35,
+  )
+  actual <- filter_ui_get_divisions(franchises)
+  expected <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    NA,        1,            30,
+    "NL",    NA,        5,            35
+  )
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_generate_division_selection single league", {
+  division_table <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    "East",    1969,         1985,
+    "AL",    NA,        1969,         2021,
+  )
+  actual <- filter_ui_generate_division_selection(division_table)
+  expected <- list("AL East (1969-1985)"="AL_East",
+                   "AL No Division"="AL_None")
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_generate_division_selection both leagues", {
+  division_table <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    "East",    1969,         1985,
+    "AL",    "West",    1980,         2021,
+    "NL",    "East",    1960,         1990
+  )
+  actual <- filter_ui_generate_division_selection(division_table)
+  expected <- list("AL Divisions"=
+                     list("AL East (1969-1985)"="AL_East",
+                          "AL West (1980-2021)"="AL_West"),
+                   "NL Divisions"=
+                     list("NL East"="NL_East")
+  )
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_generate_league_division_selection handles identical
+          year ranges", {
+  division_table <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    "East",    1969,         2021,
+    "AL",    "West",    1969,         2021
+  )
+  actual <- filter_ui_generate_league_division_selection(division_table, "AL")
+  expected <- list("AL East"="AL_East", "AL West"="AL_West")
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_generate_league_division_selection handles varying
+          year ranges", {
+  division_table <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    "East",    1969,         1985,
+    "AL",    "West",    1980,         2021,
+    "AL",    "Central", 1969,         2021
+  )
+  actual <- filter_ui_generate_league_division_selection(division_table, "AL")
+  expected <- list("AL East (1969-1985)"="AL_East",
+                   "AL West (1980-2021)"="AL_West",
+                   "AL Central"="AL_Central")
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_generate_league_division_selection handles NA division", {
+  division_table <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    "East",    1969,         1985,
+    "AL",    NA,        1969,         2021,
+  )
+  actual <- filter_ui_generate_league_division_selection(division_table, "AL")
+  expected <- list("AL East (1969-1985)"="AL_East",
+                   "AL No Division"="AL_None")
+  expect_equal(actual, expected)
+})
+
+
+test_that("filter_ui_generate_league_division_selection filters out
+          irrelevant leagues", {
+  division_table <- tibble::tribble(
+    ~League, ~Division, ~FirstSeason, ~FinalSeason,
+    "AL",    "East",    1969,         1985,
+    "AL",    "West",    1980,         2021,
+    "NL",    "East",    1960,         1990
+  )
+  actual <- filter_ui_generate_league_division_selection(division_table, "AL")
+  expected <- list("AL East (1969-1985)"="AL_East",
+                   "AL West (1980-2021)"="AL_West")
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_division_as_choice_label handles case without years
+          specified", {
+  actual <- filter_ui_division_as_choice_label("AL", "East")
+  expected <- "AL East"
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_division_as_choice_label handles case with years specified", {
+  actual <- filter_ui_division_as_choice_label("AL", "East",
+                                        1980, 1990, # First and final season
+                                        1970, 2000)  # min and max )
+  expected <- "AL East (1980-1990)"
+  expect_equal(actual, expected)
+
+})
+
+test_that("filter_ui_division_as_choice_label handles case with years at max
+          range", {
+  actual <- filter_ui_division_as_choice_label("AL", "East",
+                                        1980, 1990, # First and final season
+                                        1980, 1990)  # min and max )
+  expected <- "AL East"
+  expect_equal(actual, expected)
+
+})
+
+test_that("filter_ui_division_as_choice_label handles case with almost full
+          range", {
+  # Match the max but not the min
+  actual <- filter_ui_division_as_choice_label("AL", "East",
+                                        1980, 1990, # First and final season
+                                        1979, 1990)  # min and max )
+  expected <- "AL East (1980-1990)"
+  expect_equal(actual, expected)
+
+  # Match the min but not the max
+  actual <- filter_ui_division_as_choice_label("AL", "East",
+                                        1980, 1990, # First and final season
+                                        1980, 1991)  # min and max )
+  expected <- "AL East (1980-1990)"
+  expect_equal(actual, expected)
+})
+
+test_that("filter_ui_division_as_choice_label handles NA division", {
+  actual <- filter_ui_division_as_choice_label("AL", NA)
+  expected <- "AL No Division"
+  expect_equal(actual, expected)
+})
+
+
+
+
+
+
+
+
+
