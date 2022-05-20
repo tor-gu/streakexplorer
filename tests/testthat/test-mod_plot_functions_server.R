57dcd47f8b2d7b96d5abefe896d7d021babb1346
tor-gu <tor.gunston@protonmail.com>
2022-05-20 13:01:26 -0400

    Modularizing: ps_streaks_get_max_rank ps_streaks_get_max_rank_simple ps_streaks_get_max_rank_by_sampling

--- tests/testthat/test-mod_plot_functions_server.R
+++ tests/testthat/test-mod_plot_functions_server.R
@@ -20,3 +20,36 @@ test_that("ps_franchise_ids_to_team_ids basic test", {
   expect_equal(actual, expected)
 })
 
+test_that("ps_streaks_get_max_rank_simple basic test", {
+  with_mock_db({
+    mock_conn <- suppressWarnings(
+      dbConnect(RMySQL::MySQL(), dbname="streak_explorer_data")
+    )
+    lzy_streaks <- dplyr::tbl(mock_conn, "hot_streaks")
+    teams <- c("BOS","CIN")
+    min_year <- 1948
+    max_year <- 1949
+    actual <- ps_streaks_get_max_rank_simple(lzy_streaks, 3, min_year, max_year,
+                                             teams)
+    dbDisconnect(mock_conn)
+  })
+  expect_equal(actual, 1985)
+})
+
+test_that("ps_streaks_get_max_rank_by_sampling basic test", {
+  with_mock_db({
+    mock_conn <- suppressWarnings(
+      dbConnect(RMySQL::MySQL(), dbname="streak_explorer_data")
+    )
+    lzy_streaks <- dplyr::tbl(mock_conn, "hot_streaks")
+    teams <- c("BOS","CIN")
+    min_year <- 1948
+    max_year <- 1949
+    actual <- ps_streaks_get_max_rank_by_sampling(lzy_streaks, 3, min_year,
+                                                  max_year, teams, c(20,80), 1.3)
+    dbDisconnect(mock_conn)
+  })
+  expect_equal(actual, 1784)
+})
+
+
--- tests/testthat/test-streaks.R
+++ tests/testthat/test-streaks.R
@@ -87,38 +87,6 @@ test_that("streaks_get_standings works with 'completions'", {
   expect_equal(actual$standings_final,  expected_standings_final)
 })
 
-test_that("streaks_get_max_rank_simple basic test", {
-  with_mock_db({
-    mock_conn <- suppressWarnings(
-      dbConnect(RMySQL::MySQL(), dbname="streak_explorer_data")
-    )
-    lzy_streaks <- dplyr::tbl(mock_conn, "hot_streaks")
-    teams <- c("BOS","CIN")
-    min_year <- 1948
-    max_year <- 1949
-    actual <- streaks_get_max_rank_simple(lzy_streaks, 3, min_year, max_year,
-                                          teams)
-    dbDisconnect(mock_conn)
-  })
-  expect_equal(actual, 1985)
-})
-
-test_that("streaks_get_max_rank_by_sampling basic test", {
-  with_mock_db({
-    mock_conn <- suppressWarnings(
-      dbConnect(RMySQL::MySQL(), dbname="streak_explorer_data")
-    )
-    lzy_streaks <- dplyr::tbl(mock_conn, "hot_streaks")
-    teams <- c("BOS","CIN")
-    min_year <- 1948
-    max_year <- 1949
-    actual <- streaks_get_max_rank_by_sampling(lzy_streaks, 3, min_year,
-                                               max_year, teams, c(20,80), 1.3)
-    dbDisconnect(mock_conn)
-  })
-  expect_equal(actual, 1784)
-})
-
 test_that("streaks_get_intensity_range basic test", {
   with_mock_db({
     mock_conn <- suppressWarnings(
